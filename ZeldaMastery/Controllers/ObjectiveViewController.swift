1091a16040cd044ad784a4049b828e0a53b7fc12
Bruno Alves <alves.bruno945@gmail.com>
2024-08-24 11:33:47 -0300

    Moving LaunchingScreen and info plist to Resources folder and adding the ObjectiveViewController

---
+++ ZeldaMastery/Controllers/ObjectiveViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+final class ObjectiveViewController: UIViewController {
+
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
